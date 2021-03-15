7ded0cf53a803db07b8ed027051bd4b719eb6a81
priyapandey2020 <66728335+priyapandey2020@users.noreply.github.com>
2021-03-15 22:03:42 +0530

    Update System.js

--- js/System.js
+++ js/System.js
@@ -6,7 +6,7 @@ class System{
         textSize(50);
         fill("black");
         text(code,300,300)
-        if(actualCode === enteredCode) 
+        if(actualCode === enteredCode.toUpperCase()) 
             return true
         else
             return false
